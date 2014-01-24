71df87fe1b9fad8a0a9c59cfe5ce2ef915e785b9
rmekhail <rmekhail@gmail.com>
2014-01-23 21:18:02 -0800

    Added HomeController, GuestResponse model, and 2 new views

---
+++ MyFirstASPMVCTest/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstASPMVCTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "Good afternoon";
+            return View();
+        }
+
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+    }
+}
